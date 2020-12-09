60e916b97142bd2d19083169e3051cbb332ea44e
minjae <alswo8138@gmail.com>
2020-12-09 22:30:19 +0900

    #

---
+++ Programmers/더맵게.py
@@ -0,0 +1,15 @@
+import heapq
+def solution(scv, K): 
+    answer = 0 
+    heapq.heapify(scv) # 기존의 list를 heap구조에 담기 (자동정렬)
+
+    
+    # heappush(heap,item)
+    # heap에 item을 추가
+    while scv[0] < K: 
+        try: 
+            heapq.heappush(scv,heapq.heappop(scv)+(heapq.heappop(scv)*2))  #heappop은 heap에서 최솟값을 삭제하고 반환 
+        except IndexError: 
+            return -1 
+        answer += 1 
+    return answer
