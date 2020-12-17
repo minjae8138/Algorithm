2cf40b8500b663dac047f1064f37bd7d9e0ed6c3
minjae <alswo8138@gmail.com>
2020-12-18 00:38:23 +0900

    o

---
+++ BOJ/문문문(17210).py
@@ -0,0 +1,16 @@
+n = int(input())
+k = int(input())
+# lst = [-1] * (n+1)
+
+# for i in range(2,n+1) :
+#     if k == 0 :
+#         if 
+if n < 6 :
+    for i in range(n-1) :
+        if k == 0 :
+            print(k+1)
+            k+=1
+        elif k == 1 :
+            print(k-1)
+            k-=1
+else : print("Love is open door")
