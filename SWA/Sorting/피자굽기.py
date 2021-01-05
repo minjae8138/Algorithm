b870b1c0f0f5ebdfa79ccbafc6273b9219c48510
minjae <alswo8138@gmail.com>
2021-01-05 12:08:17 +0900

    rename

--- SWA/Sorting/피자굽기.py
+++ SWA/Sorting/피자굽기.py
@@ -23,6 +23,30 @@ for T in (1,t+1) :
             if not arr :
                 if que[i] == 0 :
                     que.pop()
-    print(que)   
+    print(que) 
+
+# from collections import deque
+
+# T = int(input())
+
+# for test_case in range(1,T+1):
+#     n,m = map(int, input().split()) # 화덕 n, 피자 m
+#     pizza = deque([(i+1,j) for i,j in enumerate(list(map(int, input().split())))])
+#     c = deque()
+
+#     for i in range(n):
+#         c.append(pizza.popleft())
+
+#     while len(c) > 1:
+#         p = c.popleft()
+#         cheeze = p[1]//2
+#         if cheeze == 0 and pizza:
+#             c.append(pizza.popleft())
+#         elif cheeze == 0 and not pizza:
+#             continue
+#         else:
+#             p = (p[0],p[1]//2)
+#             c.append(p)
+#     print(f'#{test_case}',c[0][0]) 
     
 
