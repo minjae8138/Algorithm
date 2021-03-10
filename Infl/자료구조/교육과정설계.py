fb1d1dafb72a4287557993bec9bc01964ab16277
minjae <alswo8138@gmail.com>
2021-03-11 00:41:49 +0900

    update comment

--- Infl/자료구조/교육과정설계.py
+++ Infl/자료구조/교육과정설계.py
@@ -1,3 +1,9 @@
+# deque의 장점은 시간복잡도이지만
+# 이 문제에서는 ans = deque(need)가 아닌
+# ans = need로 하고 일반 pop을 사용하면 ans의 형태유지가 되지 않기 때문에
+# 반드시 deque를 사용해야 한다
+# 왜 deque를 안쓰면 ans 형태가 초기화가 안되는지는 모르겠다..
+
 from collections import deque
 
 need = input()
