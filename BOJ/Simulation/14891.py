5ee32c7edb707aaacab34947c59e1ae3bbbc1720
minjae <alswo8138@gmail.com>
2021-03-17 08:47:23 +0900

    modify code/simulation

--- BOJ/Simulation/14891.py
+++ BOJ/Simulation/14891.py
@@ -1,53 +1,59 @@
-top_list = [list(map(int,input())) for _ in range(4)]
-t = int(input())
-linked = [0]*5
+topni = [list(map(int,input())) for _ in range(4)]
+n = int(input())
 
+for _ in range(n) :
+    which, rotate = map(int,input().split())
+    a = which + 1   # which값 초기화를 위해 만듦
+    b = rotate + 1
+    going = [[which-1,rotate]] # 회전할 톱니바퀴 체크해서 담기
 
-# 하나씩 회전하는 코드
-def rotation(lst,r,d) :
-    if d == 1 :
-        lst[r-1].insert(0,lst[r-1].pop(-1))
-    elif d == -1 :
-        lst[r-1].append(lst[r-1].pop(0))
-        # print(lst[r-1])
-    return lst
-
-for _ in range(t) :
-    chk = []
-    cnt = 0
-    def dfs(x) :
-        global chk
-        global cnt
-        cnt += 1
-        dx = [-1,0]
-
-        for i in range(2) :
-            ddx = x + dx[i]
-
-            if linked[ddx] == 1 :
-                linked[ddx] = 0
-                chk.append(ddx)
-                chk.append(ddx+1)
-                dfs(ddx)
-        return chk
-
-    r,d = map(int,input().split())
-    dfs(r)
-    for i in range(3) :
-        if top_list[i][2] != top_list[i+1][-2] :
-            linked[i+1] = 1
-    for c in chk :
-        if abs(c - r) % 2 == 0 :
-            d = 1
+    # 기준 톱니바퀴의 왼쪽부분 체크
+    while True :
+        if which-1 < 1 :
+            break
+        if topni[which-1][6] != topni[which-2][2] :
+            if rotate == 1 :
+                going.append([which-2,-1])
+                rotate = -1
+            else :
+                going.append([which-2,1])
+                rotate = 1
         else :
-            d = -1
-        rotation(top_list,c,d)
+            break
+        which -= 1
+    # which 값 초기화
+    which = a - 1
+    rotate = b - 1
 
+    # 기준 톱니바퀴의 오른쪽부분 체크
+    while True :
+        if which > 3 :
+            break
+        if topni[which][6] != topni[which-1][2] :
+            if rotate == 1 :
+                going.append([which,-1])
+                rotate = -1
+            else :
+                going.append([which,1])
+                rotate = 1
+        else :
+            break
+        which += 1
 
+    # 체크한 톱니바퀴 회전하는 코드
+    for w in going :
+        cur = topni[w[0]]
+        if w[1] == 1 :
+            cur = cur.insert(0,cur.pop())
+        elif w[1] == -1 :
+            cur = cur.append(cur.pop(0))
 
-print(top_list[i][0]*1 + top_list[1][0]*2
-      + top_list[2][0]*4 + top_list[3][0]*8)
-
-
+# print(topni)
+num = 1
+answer = 0
+for i in range(4) :
+    answer += topni[i][0] * num
+    num *= 2
+print(answer)
 
 
