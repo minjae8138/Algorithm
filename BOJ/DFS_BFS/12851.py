7e1e14f52ca3eafcd0a435e2d8ce72452c46baad
minjae <alswo8138@gmail.com>
2021-03-20 10:28:28 +0900

    update

--- BOJ/DFS_BFS/12851.py
+++ BOJ/DFS_BFS/12851.py
@@ -1,11 +1,9 @@
 
 from collections import deque
 
-
 def bfs(v):
     cnt = 0
     q = deque([[v, cnt]])
-
     while q:
         v = q.popleft()
         e = v[0]
@@ -25,8 +23,32 @@ def bfs(v):
 
     return cnt
 
-
 a, b = map(int, input().split())
 visited = [0] * 100001
 print(bfs(a))
 
+
+# 이전 풀이와 같은 bfs지만
+# 좀 더 간결하게 풀었다
+# bfs 함수의 인자 최소화
+# cnt 변수를 따로 만들지 않음
+
+# from collections import deque
+cur, target = map(int, input().split())
+
+q = deque()
+q.append(cur)
+visit = [0] * 100001
+large = 100001
+
+def bfs():
+    while q:
+        pos = q.popleft()
+        if pos == target:
+            break
+        for next in (pos + 1, pos - 1, pos * 2):
+            if 0 <= next < large and not visit[next]:
+                visit[next] = visit[pos] + 1
+                q.append(next)
+    return visit[pos]
+print(bfs())
