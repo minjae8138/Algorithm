09bc343453701a831894df1be56d175db579aec8
minjae <alswo8138@gmail.com>
2021-03-27 15:43:22 +0900

    cut_edge / dfs

--- Infl/완전탐색(상태트리)/바둑이승차.py
+++ Infl/완전탐색(상태트리)/바둑이승차.py
@@ -1,22 +1,26 @@
-weight, n = map(int,input().split())
+weight, n = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
-# print(weight,n,arr)
 max_val = 0
-def dfs(l,sum) :
+
+def dfs(l, sum, tsum):
     global max_val
-    if sum > weight :
+
+    # 남아있는 트리를 더하고 현재 max_val보다 작으면 더 계산할필요가 없음
+    if sum + (total - tsum) < max_val:  # cut edge
         return
 
-    if l == n :
-
-        if sum < weight :
-            if sum > max_val :
+    if sum > weight:  # cut edge
+        return
+    if l == n:
+        if sum < weight:
+            if sum > max_val:
                 max_val = sum
         # print(max_val)
         return max_val
-    else :
-        dfs(l+1, sum+arr[l])
-        dfs(l+1, sum)
+    else:
+        dfs(l + 1, sum + arr[l], tsum + arr[l])
+        dfs(l + 1, sum, tsum + arr[l])
     return max_val
 
-print(dfs(0,0))
+total = sum(arr)
+print(dfs(0, 0, 0))
