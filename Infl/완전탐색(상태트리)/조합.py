e35002af1343468c4493c54e3f5a49438b5b820f
minjae <alswo8138@gmail.com>
2021-04-15 00:57:01 +0900

    permutation / dfs

---
+++ Infl/완전탐색(상태트리)/조합.py
@@ -0,0 +1,16 @@
+def dfs(l,s) :
+    global cnt
+    if l==m :
+        for i in range(m) :
+            print(res[i],end=" ")
+        print()
+        cnt+=1
+    else :
+        for i in range(s,n+1) :
+            res[l] = i
+            dfs(l+1, i+1)
+n,m = map(int,input().split())
+res = [0] *(n+1)
+cnt = 0
+dfs(0,1)
+print(cnt)
