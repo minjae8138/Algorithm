2de126f6dd36e91835271e10a628b33abfba7f72
minjae <alswo8138@gmail.com>
2021-04-16 23:53:40 +0900

    update

--- Infl/완전탐색(상태트리)/조합.py
+++ Infl/완전탐색(상태트리)/조합.py
@@ -6,7 +6,7 @@ def dfs(l,s) :
         print()
         cnt+=1
     else :
-        for i in range(s,n+1) :
+        for i in range(s,n+1) :  # 조합이어서 범위가 이전값인 s부터
             res[l] = i
             dfs(l+1, i+1)
 n,m = map(int,input().split())
