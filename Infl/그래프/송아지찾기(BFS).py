c0e424b89269223138563704433eacacabe1a6a3
minjae <alswo8138@gmail.com>
2021-04-24 13:09:51 +0900

    bfs

---
+++ Infl/그래프/송아지찾기(BFS).py
@@ -0,0 +1,18 @@
+n,m = map(int,input().split())
+from collections import deque
+jump = [1,-1,5]
+dis = [0] * 10000001
+ch = [0] * 10000001
+q = deque([n])
+while True :
+    v = q.popleft()
+    ch[v] = 1
+    for i in range(3) :
+        new_v = v + jump[i]
+        if ch[new_v] == 0 :
+            ch[new_v] = 1
+            dis[new_v] = dis[v] + 1
+            q.append(new_v)
+    if dis[m] != 0 :
+        break
+print(dis[m])
