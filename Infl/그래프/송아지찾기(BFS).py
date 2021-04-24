560faf63b6436a23af557c0e4cdf52fbc56dac3f
minjae <alswo8138@gmail.com>
2021-04-24 13:15:16 +0900

    bfs

--- Infl/그래프/송아지찾기(BFS).py
+++ Infl/그래프/송아지찾기(BFS).py
@@ -1,15 +1,15 @@
 n,m = map(int,input().split())
 from collections import deque
 jump = [1,-1,5]
-dis = [0] * 10000001
-ch = [0] * 10000001
+dis = [0] * 10001
+ch = [0] * 10001
 q = deque([n])
 while True :
     v = q.popleft()
     ch[v] = 1
     for i in range(3) :
         new_v = v + jump[i]
-        if ch[new_v] == 0 :
+        if ch[new_v] == 0 and 0 < new_v <= 10001:
             ch[new_v] = 1
             dis[new_v] = dis[v] + 1
             q.append(new_v)
