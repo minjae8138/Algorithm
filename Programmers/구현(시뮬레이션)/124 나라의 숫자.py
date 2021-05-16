e59903a142ce64f210f634b9465903628d044f0f
minjae <alswo8138@gmail.com>
2021-05-16 10:46:12 +0900

    divmod

--- Programmers/구현(시뮬레이션)/124 나라의 숫자.py
+++ Programmers/구현(시뮬레이션)/124 나라의 숫자.py
@@ -1,3 +1,22 @@
+# 3진수구하는 것과 유사 (0이 4로 바뀌는 것)
+
+# 내 풀이
+def solution(n):
+    answer = []
+    while 1:
+        if n <= 3:
+            answer.append(("124"[n - 1]))
+            break
+        else:
+            n, r = divmod(n - 1, 3)
+            answer.append(("124"[r]))
+
+    answer = "".join(answer[::-1])
+
+    return answer
+
+# 다른 사람 풀이
+# 재귀 활용
 def solution(n):
     answer = ''
     if n <= 3:
@@ -7,4 +26,3 @@ def solution(n):
         return solution(q) + '124'[r]
 
 
-# 3진수구하는 것과 유사
