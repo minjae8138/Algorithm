cc8a1016b31753d7b35a6861153db479a7810b44
minjae <alswo8138@gmail.com>
2021-05-31 00:34:25 +0900

    Resolve

--- Infl/그래프/최대점수구하기(DFS).py
+++ Infl/그래프/최대점수구하기(DFS).py
@@ -1,24 +1,23 @@
-def dfs(l,s,t) :
+def dfs(l,s,t) :  # l : 노드 길이, s : 점수 합
     global res
     if t > m :
         return
     if l == n :
-        print(l,s,t,res)
+        print(l, s, t, res)
         if s > res :
             res = s
+        print("res : ",res)
+        return res
     else :
-        dfs(l+1,s+score[l],t+time[l])
-        dfs(l+1,s,t)
-
+        dfs(l+1, s+scores[l],t+times[l])
+        dfs(l+1, s, t)
 
 n,m = map(int,input().split())
-score = list()
-time = list()
+temp = [list(map(int,input().split())) for _ in range(n)]
 res = -10000
-for _ in range(n) :
-    a, b = map(int,input().split())
-    score.append(a)
-    time.append(b)
+
+scores = list(map(lambda x: x[0], temp))
+times  = list(map(lambda x: x[1], temp))
+
 dfs(0,0,0)
 print(res)
-
--- Infl/그래프/휴가(DFS).py
+++ Infl/그래프/휴가(DFS).py
@@ -1,22 +1,46 @@
-def dfs(l,sum_) :
+def dfs(l,t,p) :    # l : 노드 길이, t : time, p : pay
     global res
-    if l == n+1 :
-        if sum_ > res :
-            res = sum_
+    if t > n or l > n:
+        return
+    if l == n :
+        # print(l,t,p,res)
+        if p > res :
+            res = p
     else :
-        if l + pt[l] <= n+1 :
-            dfs(l+pt[l],sum_+pv[l])
-        dfs(l+1,sum_)
+        if l+times[l] <=n :
+            # print("before : ",l,t,p,res)
+            dfs(l+times[l], t+times[l], p+pays[l])
+        dfs(l+1,t,p)
 
 n = int(input())
-pt = list()
-pv = list()
-for _ in range(n) :
-    a,b = map(int,input().split())
-    pt.append(a)
-    pv.append(b)
-pv.insert(0,0)
-pt.insert(0,0)
-res = -1000000
-dfs(1,0)
+temp  = [list(map(int,input().split())) for _ in range(n)]
+times = list(map(lambda x:x[0],temp))
+pays  = list(map(lambda x:x[1],temp))
+
+res = -10000
+dfs(0,0,0)
 print(res)
+
+
+# def dfs(l,sum_) :
+#     global res
+#     if l == n+1 :
+#         if sum_ > res :
+#             res = sum_
+#     else :
+#         if l + pt[l] <= n+1 :
+#             dfs(l+pt[l],sum_+pv[l])
+#         dfs(l+1,sum_)
+#
+# n = int(input())
+# pt = list()
+# pv = list()
+# for _ in range(n) :
+#     a,b = map(int,input().split())
+#     pt.append(a)
+#     pv.append(b)
+# pv.insert(0,0)
+# pt.insert(0,0)
+# res = -1000000
+# dfs(1,0)
+# print(res)
