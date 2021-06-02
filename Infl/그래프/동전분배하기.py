b159cb167039280b3d6e564fa184adf3fdf242b8
minjae <alswo8138@gmail.com>
2021-06-02 23:40:02 +0900

    Resolve

--- Infl/그래프/동전분배하기.py
+++ Infl/그래프/동전분배하기.py
@@ -1,27 +1,25 @@
 def dfs(l) :
     global res
     if l == n :
+        if max(people) - min(people) < res :
+            temp = set()
+            for i in range(3) :
+                temp.add(people[i])
+            if len(temp)==3 :
+                res = max(people) - min(people)
+                print(res)
 
-        if max(money) - min(money) < res :
-            tmp = set()
-            for x in money :
-                tmp.add(x)
-            if len(tmp) == 3 :
-                res = max(money) - min(money)
-                print(money)
     else :
         for i in range(3) :
-            money[i] += arr[l]
+            people[i]+=arr[l]
             dfs(l+1)
-            money[i] -= arr[l] # 이전 가지로 돌아가는 경우 다시 빼주는 코드
+            people[i]-=arr[l]   # 이전 가지로 돌아가는 경우 다시 빼준다
+
 
 
 n = int(input())
-arr = list()
-for _ in range(n) :
-    a = int(input())
-    arr.append(a)
-money = [0] * 3
+arr = list(map(int,input().split()))
+people = [0] * 3
 res = 24700000
 dfs(0)
 print(res)
