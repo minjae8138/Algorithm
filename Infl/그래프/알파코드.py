27fe035fd9426f6d0375fa1bae35ab417720245e
minjae <alswo8138@gmail.com>
2021-06-04 08:45:54 +0900

    Resolve

--- Infl/그래프/알파코드.py
+++ Infl/그래프/알파코드.py
@@ -1,25 +1,23 @@
-def dfs(l, p):
+def dfs(l,p) :
     global cnt
     if l == n:
         cnt += 1
         for j in range(p):
             print(chr(res[j] + 64), end="")
         print()
-    else:
-        for i in range(1, 27):
-            if code[l] == i:
+    else :
+        for i in range(1,27) :
+            if code[l] == i :
                 res[p] = i
-                dfs(l + 1, p + 1)
-
+                dfs(l+1,p+1)
             elif i >= 10 and code[l] == i // 10 and code[l + 1] == i % 10:
                 res[p] = i
-                dfs(l + 2, p + 1)
+                dfs(l+2,p+1)
 
-
-code = list(map(int, input()))
+code = list(map(int,input()))
 n = len(code)
 code.insert(n, -1)
-res = [0] * (n + 3)
+res = [0] * (n+3)
 cnt = 0
-dfs(0, 0)
+dfs(0,0)
 print(cnt)
