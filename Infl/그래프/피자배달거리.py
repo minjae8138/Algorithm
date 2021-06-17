d2bb0bc950ad1521f034ab35685301a76d5428a7
minjae <alswo8138@gmail.com>
2021-06-17 22:58:36 +0900

    Modify comment

--- Infl/그래프/피자배달거리.py
+++ Infl/그래프/피자배달거리.py
@@ -6,7 +6,7 @@ city = copy.deepcopy(matrix)
 hs = []     # 집
 pz = []     # 피자집
 cb = [0] * m
-res = 24700000
+res = 2147000000
 for i in range(n) :
     for j in range(n) :
         if city[i][j] == 1 :
@@ -20,22 +20,22 @@ print(pz)
 def dfs(l,s) :
     global res
     if l == m :
-        tot= 0      # 도시의 피자배달 거리
+        tot= 0      # 도시의 피자배달 거리 (집별 피자배달 최소거리의 합)
         for i in range(len(hs)) :
             x1 = hs[i][0]
             y1 = hs[i][1]
-            dis = 24700000      # 집별 피자배달 거리
+            dis = 2147000000      # 집별 피자배달 거리
             for c in cb :
                 x2 = pz[c][0]
                 y2 = pz[c][1]
-                dis = min(dis,abs(x1-x2) + abs(y1-y2))
+                dis = min(dis,abs(x1-x2) + abs(y1-y2))  # 최소거리
             tot += dis
         if tot <= res :
             res = tot
 
     else :
         # 피자집의 조합구하기
-        for i in range(s, len(pz)) :
+        for i in range(s, len(pz)) :    # 같은 길이 유지하면서 가기 위해 s부터~
            cb[l] = i
            dfs(l+1, i+1)
 
