98cb2829229ae7c1fecc33d9170ab1f4858d3c8b
MJ <abc@gmail.com>
2021-06-27 00:17:34 +0900

    .

--- Programmers/정렬/가장큰수.py
+++ Programmers/정렬/가장큰수.py
@@ -1,3 +1,3 @@
 def solution(numbers):
     sort_num = sorted(list(map(str, numbers)), key=lambda x: x * 3, reverse=True)
-    return str(int("".join(sort_num)))      # [0,0,0] -> "0" 처럼 0으로 이루어진 케이스 때문에 int를 거쳐서 str로 변환해야함
+    return str(int("".join(sort_num)))          # [0,0,0] -> "0" 처럼 0으로 이루어진 케이스 때문에 int를 거쳐서 str로 변환해야함
